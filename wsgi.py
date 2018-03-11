3e93bffeadf4259553f7ec716b3853ec17875cf7
Paul Ronga <paul.ronga@laposte.net>
2018-03-11 22:50:06 +0100

    temp wsgi file without fb implementation

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from fbmq import Page
+#from fbmq import Page
 from os import environ
 
 app = Flask(__name__)
@@ -12,29 +12,35 @@ except NameError:
     print("Access token was NOT defined!")
     PAGE_ACCESS_TOKEN = 'dummy'
     
-page = Page(PAGE_ACCESS_TOKEN)
+#page = Page(PAGE_ACCESS_TOKEN)
 
 @app.route("/")
 def hello():
     return "Hello World!"
-    
-@app.route('/webhook', methods=['POST'])
+
+@app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    page.handle_webhook(request.get_data(as_text=True))
+    #page.handle_webhook(request.get_data(as_text=True))
     return "ok"
 
-@page.handle_message
-def message_handler(event):
-    """:type event: fbmq.Event"""
-    sender_id = event.sender_id
-    message = event.message_text
-
-    page.send(sender_id, "merci! votre message est '%s'" % message)
-
-@page.after_send
-def after_send(payload, response):
-    """:type payload: fbmq.Payload"""
-    print("complete")
+    
+# @app.route('/webhook', methods=['POST'])
+# def webhook():
+#     page.handle_webhook(request.get_data(as_text=True))
+#     return "ok"
+# 
+# @page.handle_message
+# def message_handler(event):
+#     """:type event: fbmq.Event"""
+#     sender_id = event.sender_id
+#     message = event.message_text
+# 
+#     page.send(sender_id, "merci! votre message est '%s'" % message)
+# 
+# @page.after_send
+# def after_send(payload, response):
+#     """:type payload: fbmq.Payload"""
+#     print("complete")
   
 if __name__ == "__main__":
     app.run()
