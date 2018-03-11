0aa4f082055b9e311c5386e091331746151902d3
Paul Ronga <paul.ronga@laposte.net>
2018-03-11 19:44:18 +0100

    update webhook script

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,22 @@
-from flask import Flask
-application = Flask(__name__)
+from flask import Flask, request
+from fbmq import Page
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+page = Page(PAGE_ACCESS_TOKEN)
 
-if __name__ == "__main__":
-    application.run()
+@app.route('/webhook', methods=['POST'])
+def webhook():
+  page.handle_webhook(request.get_data(as_text=True))
+  return "ok"
+
+@page.handle_message
+def message_handler(event):
+  """:type event: fbmq.Event"""
+  sender_id = event.sender_id
+  message = event.message_text
+  
+  page.send(sender_id, "merci! votre message est '%s'" % message)
+
+@page.after_send
+def after_send(payload, response):
+  """:type payload: fbmq.Payload"""
+  print("complete")
