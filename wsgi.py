27120ead2b94e4513dd19d71b7ae4058bd5df613
Paul Ronga <paul.ronga@laposte.net>
2018-03-11 20:58:16 +0100

    update wsgi

--- wsgi.py
+++ wsgi.py
@@ -9,21 +9,21 @@ def hello():
     
 @app.route('/webhook', methods=['POST'])
 def webhook():
-  page.handle_webhook(request.get_data(as_text=True))
-  return "ok"
+    page.handle_webhook(request.get_data(as_text=True))
+    return "ok"
 
 @page.handle_message
 def message_handler(event):
-  """:type event: fbmq.Event"""
-  sender_id = event.sender_id
-  message = event.message_text
-  
-  page.send(sender_id, "merci! votre message est '%s'" % message)
+    """:type event: fbmq.Event"""
+    sender_id = event.sender_id
+    message = event.message_text
+
+    page.send(sender_id, "merci! votre message est '%s'" % message)
 
 @page.after_send
 def after_send(payload, response):
-  """:type payload: fbmq.Payload"""
-  print("complete")
+    """:type payload: fbmq.Payload"""
+    print("complete")
   
- if __name__ == "__main__":
-     application.run()
+if __name__ == "__main__":
+    application.run()
